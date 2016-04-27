8db6a8bac55becd20bf01889fd04a8f103e54de9
Sun Yaozhu <yzyzsun@gmail.com>
2016-04-28 00:43:24 +0800

    Update gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   private
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up)        << :sid << :name << :school_class
-      devise_parameter_sanitizer.for(:account_update) << :sid << :name << :school_class
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:sid, :name, :school_class])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:sid, :name, :school_class])
     end
 
     def admin_user
