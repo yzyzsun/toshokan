1369697aed5076ad65e28b7bb3e77383fc49bc7c
Sun Yaozhu <yzyzsun@gmail.com>
2016-04-28 00:07:53 +0800

    Finish borrowing and returning

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :admin_user, only: [:new, :create, :edit, :update, :destroy]
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :set_book, only: [:show, :edit, :update, :destroy, :borrow, :return]
   before_action :set_categories, only: [:index, :new, :edit]
 
   # GET /books
@@ -46,6 +46,31 @@ class BooksController < ApplicationController
   def destroy
     @book.destroy
     redirect_to root_url, notice: '图书删除成功'
+  end
+
+  # GET /books/1/borrow
+  def borrow
+    if @book.user.nil?
+      @book.user = current_user
+      @book.last_borrowed_at = Time.zone.now
+      @book.borrow_count += 1
+      @book.save
+      redirect_to @book, notice: '图书借阅成功'
+    else
+      redirect_to @book, alert: '图书已被他人借阅'
+    end
+  end
+
+  # GET /books/1/return
+  def return
+    if @book.user == current_user
+      @book.user = nil
+      @book.last_returned_at = Time.zone.now
+      @book.save
+      redirect_to @book, notice: '图书归还成功'
+    else
+      redirect_to @book, alert: '您不是图书当前的借阅者'
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
   devise_for :users
   resources :users, only: [:index, :show]
-  resources :books, except: :index
+  resources :books, except: :index do
+    get 'borrow', on: :member
+    get 'return', on: :member
+  end
   resources :categories, except: [:index, :edit, :update]
 end
